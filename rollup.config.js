e684bddca90ebb7a976b6595af5245405d6bdb76
microshine <microshine@mail.ru>
2020-03-15 14:10:34 +0300

    Update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,40 @@
-import { ts, dts } from "rollup-plugin-dts";
-// import builtins from "rollup-plugin-node-builtins";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
-const pkg = require("./package.json");
 const external = [...Object.keys(pkg.dependencies)];
 const input = "src/index.ts";
 
-export default [
-  { // CommonJS
-    input,
-    plugins: [
-      ts({
+const banner = [
+  "/**",
+  " * Copyright (c) 2020, Peculiar Ventures, All rights reserved.",
+  " */",
+  "",
+].join("\n");
+
+export default {
+  input,
+  plugins: [
+    typescript({
+      check: true,
+      clean: true,
+      tsconfigOverride: {
         compilerOptions: {
-          removeComments: true,
-        },
-      }),
-    ],
-    external,
-    output: [
-      {
-        file: pkg.main,
-        format: "cjs",
-      },
-      {
-        file: pkg.module,
-        format: "es",
-      }
-    ]
-  },
-  { // Definitions
-    input,
-    plugins: [
-      dts({
-        compilerOptions: {
-          noResolve: true,
+          module: "ES2015",
         }
-      }),
-    ],
-    external,
-    output: [
-      {
-        file: pkg.types,
-        format: "es",
       }
-    ]
-  },
-];
+    }),
+  ],
+  external,
+  output: [
+    {
+      banner,
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      banner,
+      file: pkg.module,
+      format: "es",
+    },
+  ],
+};
