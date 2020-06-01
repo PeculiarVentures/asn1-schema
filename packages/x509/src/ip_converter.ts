2513e139227d9152ee0cd33a7ef257c6eec61cb2
microshine <microshine@mail.ru>
2020-06-01 11:57:48 +0300

    chore(x509): Stringify IPV4 with mask #14

--- packages/x509/src/ip_converter.ts
+++ packages/x509/src/ip_converter.ts
@@ -1,13 +1,40 @@
 import * as ip from "ipaddr.js";
+import { Convert } from "pvtsutils";
 
 export class IpConverter {
-  public static toString(buf: ArrayBuffer) {
-    const uint8 = new Uint8Array(buf);
-    const addr = ip.fromByteArray(Array.from(uint8));
-    return addr.toString();
+
+  private static decodeIP(value: string) {
+    if (value.length === 64 && parseInt(value, 16) === 0) {
+      return "::/0";
+    }
+
+    if (value.length !== 16) {
+      return value;
+    }
+
+    const mask = parseInt(value.slice(8), 16)
+      .toString(2)
+      .split('')
+      .reduce((a, k) => a + (+k), 0);
+    let ip = value.slice(0, 8)
+      .replace(/(.{2})/g, match => `${parseInt(match, 16)}.`);
+
+    ip = ip.slice(0, -1);
+
+    return `${ip}/${mask}`;
   }
+
+  public static toString(buf: ArrayBuffer) {
+    if (buf.byteLength === 4 || buf.byteLength === 16) {
+      const uint8 = new Uint8Array(buf);
+      const addr = ip.fromByteArray(Array.from(uint8));
+      return addr.toString();
+    }
+    return this.decodeIP(Convert.ToHex(buf));
+  }
+
   public static fromString(text: string): ArrayBuffer {
-    const addr = ip.parse(text);
+    const addr = ip.parse(text) as any;
     return new Uint8Array(addr.toByteArray()).buffer;
   }
 }
