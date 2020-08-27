e06c7ee23affdb586a3f285285024f917bff06ae
microshine <microshine@mail.ru>
2020-08-27 11:08:30 +0300

    chore(cms): export OIDs

--- packages/cms/src/index.ts
+++ packages/cms/src/index.ts
@@ -3,6 +3,7 @@ export * from "./certificate_choices";
 export * from "./content_info";
 export * from "./encapsulated_content_info";
 export * from "./issuer_and_serial_number";
+export * from "./object_identifiers";
 export * from "./revocation_info_choice";
 export * from "./signed_data";
 export * from "./signer_identifier";
