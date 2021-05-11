ef3d77f0583437d5253de6c2a197ac21b12ba9a6
microshine <microshine@mail.ru>
2021-05-12 00:49:42 +0300

    style: Add comment

--- packages/rsa/src/object_identifiers.ts
+++ packages/rsa/src/object_identifiers.ts
@@ -61,6 +61,12 @@ export const id_sha1WithRSAEncryption = `${id_pkcs_1}.5`;
  * ```
  */
 export const id_sha224WithRSAEncryption = `${id_pkcs_1}.14`;
+/**
+ * ```
+ * sha224WithRSAEncryption      OBJECT IDENTIFIER ::= { pkcs-1 14 }
+ * ```
+ * @deprecated Should be removed later
+ */
 export const id_ssha224WithRSAEncryption = id_sha224WithRSAEncryption;
 
 /**
