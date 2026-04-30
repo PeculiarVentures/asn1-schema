5b240d4d73a53376fc647107e283a78afa994d70
Donskov Donskov <donskovdmitriyv@gmail.com>
2026-04-30 14:35:31 +0300

    chore: migrate testing framework from Jest to Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-unresolved
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    include: ["packages/**/*.spec.ts", "packages/**/test/**/*.ts"],
+    globals: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov", "text-summary"],
+      include: ["packages/**/*.ts", "!packages/**/*.spec.ts"],
+      exclude: ["**/*.d.ts"],
+    },
+  },
+});
