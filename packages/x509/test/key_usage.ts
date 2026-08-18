1e0143d4cd57fb9142346f2d2574f865ccafe751
Unknown-Robot <carondamien77@hotmail.fr>
2026-08-18 17:46:12 +0200

    Fix the formatting of test file

--- packages/x509/test/key_usage.ts
+++ packages/x509/test/key_usage.ts
@@ -4,78 +4,85 @@ import * as assert from "node:assert";
 describe("KeyUsage", () => {
   describe("toJSON", () => {
     it("Should return an empty array when no flags are set", () => {
-      const ku = new KeyUsage(0);
-      assert.deepStrictEqual(ku.toJSON(), []);
+      const keyUsage = new KeyUsage(0);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), []);
     });
 
     it("Should return ['digitalSignature'] for digitalSignature flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.digitalSignature);
-      assert.deepStrictEqual(ku.toJSON(), ["digitalSignature"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.digitalSignature);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["digitalSignature"]);
     });
 
     it("Should return ['nonRepudiation'] for nonRepudiation flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.nonRepudiation);
-      assert.deepStrictEqual(ku.toJSON(), ["nonRepudiation"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.nonRepudiation);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["nonRepudiation"]);
     });
 
     it("Should return ['keyEncipherment'] for keyEncipherment flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.keyEncipherment);
-      assert.deepStrictEqual(ku.toJSON(), ["keyEncipherment"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.keyEncipherment);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["keyEncipherment"]);
     });
 
     it("Should return ['dataEncipherment'] for dataEncipherment flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.dataEncipherment);
-      assert.deepStrictEqual(ku.toJSON(), ["dataEncipherment"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.dataEncipherment);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["dataEncipherment"]);
     });
 
     it("Should return ['keyAgreement'] for keyAgreement flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.keyAgreement);
-      assert.deepStrictEqual(ku.toJSON(), ["keyAgreement"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.keyAgreement);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["keyAgreement"]);
     });
 
     it("Should return ['keyCertSign'] for keyCertSign flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.keyCertSign);
-      assert.deepStrictEqual(ku.toJSON(), ["keyCertSign"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.keyCertSign);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["keyCertSign"]);
     });
 
     it("Should return ['cRLSign'] for cRLSign flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.cRLSign);
-      assert.deepStrictEqual(ku.toJSON(), ["cRLSign"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.cRLSign);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["cRLSign"]);
     });
 
     it("Should return ['encipherOnly'] for encipherOnly flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.encipherOnly);
-      assert.deepStrictEqual(ku.toJSON(), ["encipherOnly"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.encipherOnly);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["encipherOnly"]);
     });
 
     it("Should return ['decipherOnly'] for decipherOnly flag", () => {
-      const ku = new KeyUsage(KeyUsageFlags.decipherOnly);
-      assert.deepStrictEqual(ku.toJSON(), ["decipherOnly"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.decipherOnly);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["decipherOnly"]);
     });
 
     it("Should return multiple flags when combined", () => {
-      const ku = new KeyUsage(
-        KeyUsageFlags.digitalSignature
-        | KeyUsageFlags.keyCertSign
-        | KeyUsageFlags.cRLSign,
-      );
-      assert.deepStrictEqual(ku.toJSON(), ["cRLSign", "digitalSignature", "keyCertSign"]);
+      const keyUsage = new KeyUsage(KeyUsageFlags.digitalSignature | KeyUsageFlags.keyCertSign | KeyUsageFlags.cRLSign);
+
+      assert.deepStrictEqual(keyUsage.toJSON(), ["cRLSign", "digitalSignature", "keyCertSign"]);
     });
 
     it("Should return all flags when all bits are set", () => {
       const allFlags =
-        KeyUsageFlags.digitalSignature
-        | KeyUsageFlags.nonRepudiation
-        | KeyUsageFlags.keyEncipherment
-        | KeyUsageFlags.dataEncipherment
-        | KeyUsageFlags.keyAgreement
-        | KeyUsageFlags.keyCertSign
-        | KeyUsageFlags.cRLSign
-        | KeyUsageFlags.encipherOnly
-        | KeyUsageFlags.decipherOnly;
-      const ku = new KeyUsage(allFlags);
+        KeyUsageFlags.digitalSignature |
+        KeyUsageFlags.nonRepudiation |
+        KeyUsageFlags.keyEncipherment |
+        KeyUsageFlags.dataEncipherment |
+        KeyUsageFlags.keyAgreement |
+        KeyUsageFlags.keyCertSign |
+        KeyUsageFlags.cRLSign |
+        KeyUsageFlags.encipherOnly |
+        KeyUsageFlags.decipherOnly;
+      const keyUsage = new KeyUsage(allFlags);
 
-      assert.deepStrictEqual(ku.toJSON(), [
+      assert.deepStrictEqual(keyUsage.toJSON(), [
         "cRLSign",
         "dataEncipherment",
         "decipherOnly",
@@ -91,19 +98,15 @@ describe("KeyUsage", () => {
 
   describe("toString", () => {
     it("Should return formatted string with flag names", () => {
-      const ku = new KeyUsage(
-        KeyUsageFlags.keyAgreement
-        | KeyUsageFlags.encipherOnly
-        | KeyUsageFlags.decipherOnly,
-      );
+      const keyUsage = new KeyUsage(KeyUsageFlags.keyAgreement | KeyUsageFlags.encipherOnly | KeyUsageFlags.decipherOnly);
 
-      assert.strictEqual(ku.toString(), "[decipherOnly, encipherOnly, keyAgreement]");
+      assert.strictEqual(keyUsage.toString(), "[decipherOnly, encipherOnly, keyAgreement]");
     });
 
     it("Should return empty brackets when no flags are set", () => {
-      const ku = new KeyUsage(0);
+      const keyUsage = new KeyUsage(0);
 
-      assert.strictEqual(ku.toString(), "[]");
+      assert.strictEqual(keyUsage.toString(), "[]");
     });
   });
 });
